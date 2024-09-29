999f55af00884cc67f3e82c22242bd4326fd9811
ni30-rjpt <stnitish01@gmail.com>
2024-09-29 16:44:08 +0530

    initial Commint

---
+++ server/controllers/common/feature-controller.js
@@ -0,0 +1,69 @@
+const Feature = require("../../models/Feature");
+
+const addFeatureImage = async (req, res) => {
+  try {
+    const { image } = req.body;
+
+    console.log(image, "image");
+
+    const featureImages = new Feature({
+      image,
+    });
+
+    await featureImages.save();
+
+    res.status(201).json({
+      success: true,
+      data: featureImages,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+const getFeatureImages = async (req, res) => {
+  try {
+    const images = await Feature.find({});
+
+    res.status(200).json({
+      success: true,
+      data: images,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+const deleteFeatureImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Image not found",
+      });
+
+    res.status(200).json({
+      success: true,
+      message: "image delete successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+module.exports = { addFeatureImage, getFeatureImages, deleteFeatureImage };
---
+++ server/models/Feature.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const FeatureSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Feature", FeatureSchema);
